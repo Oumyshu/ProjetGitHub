3ee2e4294149fd4367c134549b3825f1065afd81
Oumyshu <oumyndong3@gmail.com>
2020-11-26 13:18:48 +0000

    modificationOumy

--- src/main/java/Calculator/unitTest/Calculator.java
+++ src/main/java/Calculator/unitTest/Calculator.java
@@ -3,39 +3,6 @@ package Calculator.unitTest;
 
 public class Calculator {
 
-    public int sum(int a, int b) {
-        return a + b;
-    }
-
-    public int minus(int a, int b) {
-        return a - b;
-    }
-
-    public int divide(int a, int b) {
-        return a / b;
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    public int Min(int a, int b) {
-        if(a<b)
-            return a;
-        else
-            return b;
-    }
-    public int Minimum(int a, int b) {
-        return Math.min(a,b);
-    }
-    public int Maximum(int a, int b) {
-        return Math.max(a,b);
-    }
-    public int Max(int a, int b) {
-        if(a<b)
-            return b;
-        else
-            return a;
-    }
     public int minElement(int[] list) {
         int min = list[0];
         int i;
